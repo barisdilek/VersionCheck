d1417b3055ad82f6de6e076e6737d56279158671
Baris <Baris@D-BARIS-I7>
2022-02-10 11:43:37 +0100

    Add project files.

---
+++ Controllers/VersionController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VersionCheck.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VersionController : ControllerBase
+    {
+        public IConfiguration Configuration { get; }
+
+        public VersionController(IConfiguration _configuration)
+        {
+            Configuration = _configuration;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok($"Api Version: {Configuration.GetSection("ApiVersion").Value}");
+        }
+    }
+}
